3d303e06f94e2212233397ca1fed8f1c0c634458
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2021-06-30 21:42:53 -0700

    Pyramid program in java

--- NewPatternProgram.java
+++ NewPatternProgram.java
@@ -4,15 +4,16 @@ public class NewPatternProgram {
     public static void main(String[] args){
         for (int i=1;i<=5;i++)
         {
-            for (int j=1;j<=5;j++)
+            for (int j=1;j<=9;j++)
             {
-               if(j>=6-i)
+               if(j>=6-i && j<=4+i)
                {
                    System.out.print("*");
                }else
                    System.out.print(" ");
             }
             System.out.println();
+
         }
 
     }
