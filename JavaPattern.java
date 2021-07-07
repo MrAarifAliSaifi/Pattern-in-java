3a43a1893c744d5b62dbdb4c92570adc26e043b8
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2021-07-06 22:13:13 -0700

    PyramidStarPattern

---
+++ JavaPattern.java
@@ -0,0 +1,19 @@
+package Com.company.satr;
+public class JavaPattern {
+    public static void main(String[] args) {
+        for (int i=1;i<=5;i++)
+        {
+            for (int j=1;j<=10;j++)
+            {
+               if(j>=6-i&&j<=4+i)
+               {
+                   System.out.print("*");
+               }else
+                   System.out.print(" ");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
