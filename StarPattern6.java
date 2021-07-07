eae8ff1d91f3f3878104bf0ff80774458df75bc0
Aarif Ali Saifi <67059471+MrAarifAliSaifi@users.noreply.github.com>
2021-07-06 22:45:22 -0700

    StarPattern_6

---
+++ StarPattern6.java
@@ -0,0 +1,27 @@
+package Com.company.satr;
+
+import javax.xml.transform.Source;
+import java.sql.SQLOutput;
+
+public class StarPattern6 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            int k=1;
+            for (int j = 1; j <= 7; j++) {
+                if (j >= 5 - i && j <= 3 + i) {
+                    System.out.print(k);
+                  if(j<4)
+                  {
+                      k++;
+                  }
+                  else
+                  {
+                      k--;
+                  }
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }}
